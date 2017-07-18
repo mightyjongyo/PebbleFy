1c7be50c3c0e67ec65707bbaafe54e8f6e0f77f8
Clement Lan <clement.lan@gmail.com>
2017-07-18 01:43:02 -0700

    remove play functions, add pebblekit

--- app/src/main/java/com/clementlan/pebblefy/MainActivity.java
+++ app/src/main/java/com/clementlan/pebblefy/MainActivity.java
@@ -4,60 +4,26 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
+import android.view.Gravity;
 import android.widget.LinearLayout;
 
+import com.getpebble.android.kit.PebbleKit;
+import com.getpebble.android.kit.util.PebbleDictionary;
 import com.spotify.sdk.android.authentication.AuthenticationClient;
 import com.spotify.sdk.android.authentication.AuthenticationRequest;
 import com.spotify.sdk.android.authentication.AuthenticationResponse;
-import com.spotify.sdk.android.player.Config;
 import com.spotify.sdk.android.player.ConnectionStateCallback;
 import com.spotify.sdk.android.player.Error;
-import com.spotify.sdk.android.player.Player;
 import com.spotify.sdk.android.player.PlayerEvent;
-import com.spotify.sdk.android.player.Spotify;
 import com.spotify.sdk.android.player.SpotifyPlayer;
 
 // Imports for Spotify Web API
+import java.util.UUID;
+
 import kaaes.spotify.webapi.android.SpotifyApi;
 import kaaes.spotify.webapi.android.SpotifyService;
-import kaaes.spotify.webapi.android.SpotifyError;
-import kaaes.spotify.webapi.android.annotations.DELETEWITHBODY;
-import kaaes.spotify.webapi.android.models.Album;
-import kaaes.spotify.webapi.android.models.Albums;
-import kaaes.spotify.webapi.android.models.AlbumsPager;
-import kaaes.spotify.webapi.android.models.Artist;
-import kaaes.spotify.webapi.android.models.Artists;
-import kaaes.spotify.webapi.android.models.ArtistsCursorPager;
-import kaaes.spotify.webapi.android.models.ArtistsPager;
-import kaaes.spotify.webapi.android.models.AudioFeaturesTrack;
-import kaaes.spotify.webapi.android.models.AudioFeaturesTracks;
-import kaaes.spotify.webapi.android.models.CategoriesPager;
-import kaaes.spotify.webapi.android.models.Category;
-import kaaes.spotify.webapi.android.models.FeaturedPlaylists;
-import kaaes.spotify.webapi.android.models.NewReleases;
 import kaaes.spotify.webapi.android.models.Pager;
-import kaaes.spotify.webapi.android.models.Playlist;
-import kaaes.spotify.webapi.android.models.PlaylistFollowPrivacy;
 import kaaes.spotify.webapi.android.models.PlaylistSimple;
-import kaaes.spotify.webapi.android.models.PlaylistTrack;
-import kaaes.spotify.webapi.android.models.PlaylistsPager;
-import kaaes.spotify.webapi.android.models.Recommendations;
-import kaaes.spotify.webapi.android.models.Result;
-import kaaes.spotify.webapi.android.models.SavedAlbum;
-import kaaes.spotify.webapi.android.models.SavedTrack;
-import kaaes.spotify.webapi.android.models.SeedsGenres;
-import kaaes.spotify.webapi.android.models.SnapshotId;
-import kaaes.spotify.webapi.android.models.Track;
-import kaaes.spotify.webapi.android.models.Tracks;
-import kaaes.spotify.webapi.android.models.TracksPager;
-import kaaes.spotify.webapi.android.models.TracksToRemove;
-import kaaes.spotify.webapi.android.models.TracksToRemoveWithPosition;
-import kaaes.spotify.webapi.android.models.UserPrivate;
-import kaaes.spotify.webapi.android.models.UserPublic;
 import retrofit.Callback;
 import retrofit.RetrofitError;
 import retrofit.client.Response;
@@ -65,13 +31,15 @@ import retrofit.client.Response;
 public class MainActivity extends Activity implements
         SpotifyPlayer.NotificationCallback, ConnectionStateCallback
 {
+    private static final UUID PEBBLE_UUID = UUID.fromString("3ef8524d-6493-49af-b014-90243f5fba61");
     private static final String CLIENT_ID = "8d8624c00f8f48da862bf7447286e708";
     private static final String REDIRECT_URI = "pebblefy-login://callback";
     // Request code that will be used to verify if the result comes from correct activity
     // Can be any integer
     private static final int LOGIN_REQUEST_CODE = 1004;
 
-    private Player mPlayer;
+    // Add back in if we need the player
+    //private Player mPlayer;
     private SpotifyApi mApi;
 
     @Override
@@ -81,12 +49,21 @@ public class MainActivity extends Activity implements
 
         this.mApi = new SpotifyApi();
 
+        /*
         // Hook up the checkbox listener
         CheckBox shufflebox = (CheckBox) findViewById(R.id.shuffle_checkbox);
         shufflebox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
                 mPlayer.setShuffle(null, b);
+            }
+        });
+        // Hook up the checkbox listener
+        CheckBox repeatbox = (CheckBox) findViewById(R.id.repeatCheckbox);
+        repeatbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                mPlayer.setRepeat(null, b);
             }
         });
 
@@ -105,6 +82,7 @@ public class MainActivity extends Activity implements
                 mPlayer.skipToNext((Player.OperationCallback) MainActivity.this);
             }
         });
+        */
 
         AuthenticationRequest.Builder builder = new AuthenticationRequest.Builder(CLIENT_ID,
                 AuthenticationResponse.Type.TOKEN,
@@ -113,7 +91,6 @@ public class MainActivity extends Activity implements
         AuthenticationRequest request = builder.build();
 
         AuthenticationClient.openLoginActivity(this, LOGIN_REQUEST_CODE, request);
-
     }
 
     @Override
@@ -123,8 +100,14 @@ public class MainActivity extends Activity implements
         if (requestCode == LOGIN_REQUEST_CODE) {
             AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, intent);
             if (response.getType() == AuthenticationResponse.Type.TOKEN) {
+                // Send the token to the pebble app
+                PebbleDictionary pebble_dict = new PebbleDictionary();
+                pebble_dict.addString(PebbleAppKeys.AccessToken, response.getAccessToken());
+                PebbleKit.sendDataToPebble(getApplicationContext(), PEBBLE_UUID, pebble_dict);
+
                 // Set up the web API with the oAuth token
                 this.mApi.setAccessToken(response.getAccessToken());
+                /* We don't really need the player anymore... wooo....
                 Config playerConfig = new Config(this, response.getAccessToken(), CLIENT_ID);
                 Spotify.getPlayer(playerConfig, this, new SpotifyPlayer.InitializationObserver() {
                     @Override
@@ -141,13 +124,15 @@ public class MainActivity extends Activity implements
                         Log.e("MainActivity", "Could not initialize player: " + throwable.getMessage());
                     }
                 });
+                */
             }
         }
     }
 
     @Override
     protected void onDestroy() {
-        Spotify.destroyPlayer(this);
+        // Add back in if we need the player after all
+        //Spotify.destroyPlayer(this);
         super.onDestroy();
     }
 
@@ -185,15 +170,22 @@ public class MainActivity extends Activity implements
                     Log.d("*** playlist found: ", plist.name);
                     PlaylistTextView p_tv = new PlaylistTextView(getApplicationContext());
                     p_tv.setPlaylist(plist);
-                    p_tv.setClickable(true);
+                    //p_tv.setClickable(true);
                     p_tv.setText(plist.name);
+                    /*
                     p_tv.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View v) {
                             mPlayer.playUri(null, ((PlaylistTextView) (v)).getPlaylistURI(), 0, 0);
                         }
                     });
+                    */
                     layout.addView(p_tv);
+                    // Before we move on, we need to set the gravity and other layout params for this list.
+                    LinearLayout.LayoutParams lp = (LinearLayout.LayoutParams) p_tv.getLayoutParams();
+                    lp.gravity = Gravity.FILL;
+                    lp.weight = 1;
+                    p_tv.setLayoutParams(lp);
                 }
             }
 
---
+++ app/src/main/java/com/clementlan/pebblefy/PebbleAppKeys.java
@@ -0,0 +1,9 @@
+package com.clementlan.pebblefy;
+
+/**
+ * Created by Clement Lan on 7/18/2017.
+ */
+
+class PebbleAppKeys {
+    public static final int AccessToken = 0;
+}
